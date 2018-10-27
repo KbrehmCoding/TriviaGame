f0ff9fabfed854b54b61777f3adfffb5e5a85c41
Krista Brehm <kristabrehm1@gmail.com>
2018-10-26 22:20:46 -0400

    reorganized the js, added jquery link, made timer stop when you submit the quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,171 +1,147 @@
-//shows a numerical value for how many were correct
-function showResults(){}
-(function() {
-    function buildQuiz() {
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+let intervalId;
 
-        const output = [];
-
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-
-        const answers = [];
-
-        for (letter in currentQuestion.answers) {
-            answers.push(
-            `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-            </label>`
-            );
-        }
-
-        output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join("")} </div>`
-        );
-        });
-
-        quizContainer.innerHTML = output.join("");
-    }
-    //shows results by changing the color of text based on which ones are right  or wrong
-    function showResults() {
-
-        const answerContainers = quizContainer.querySelectorAll(".answers");
-
-
-        let numCorrect = 0;
-
-
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-
-        if (userAnswer === currentQuestion.correctAnswer) {
-
-            numCorrect++;
-
-            answerContainers[questionNumber].style.color = "lightgreen";
-        } else {
-
-            answerContainers[questionNumber].style.color = "red";
-        }
-        });
-
-        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-    //data for questions and answers
-    const quizContainer = document.getElementById("quiz");
-    const resultsContainer = document.getElementById("results");
-    const submitButton = document.getElementById("submit");
-    const myQuestions = [
-        {
+const myQuestions = [
+    {
         question: "Which Jane Austen Novel had a charcter named Mr. Darcy?",
         answers: {
-        a: "Sense and Sensibility",
-        b: "Emma",
-        c: "Pride and Prejidice",
-        d: "Persusion",
+            a: "Sense and Sensibility",
+            b: "Emma",
+            c: "Pride and Prejudice",
+            d: "Persusion",
         },
-        correctAnswer : "c"
+        correctAnswer: "c"
     },
     {
         question: "Mark Twains real name was?",
         answers: {
-        a: "Adam Clark",
-        b: "John Birch",
-        c: "Samuel Clemins",
-        d: "Andrew Roberts",
+            a: "Adam Clark",
+            b: "John Birch",
+            c: "Samuel Clemins",
+            d: "Andrew Roberts",
         },
-        correctAnswer : "c"
+        correctAnswer: "c"
     },
     {
-
-        question: "What was Stephen Kings first published novel?",
+        question: "What was Stephen King's first published novel?",
         answers: {
-        a: "Carrie",
-        b: "I.T.",
-        c: "The Green Mile",
-        d: "Under The Dome",
+            a: "Carrie",
+            b: "I.T.",
+            c: "The Green Mile",
+            d: "Under The Dome",
         },
-        correctAnswer : "a"
+        correctAnswer: "a"
     },
     {
-
-    question: "In The Percy Jackson and the Olypians, Percy was the son of which Greek God?",
-        answers:{
-        a: "Posidon",
-        b: "Zeus",
-        c: "Hermes",
-        d: "Aries",
+        question: "In The Percy Jackson and the Olypians, Percy was the son of which Greek God?",
+        answers: {
+            a: "Posidon",
+            b: "Zeus",
+            c: "Hermes",
+            d: "Aries",
         },
-        correctAnswer : "a"
+        correctAnswer: "a"
     },
     {
-
         question: "H.G. Wells wrote primarily in what genre?",
         answers: {
-        a: "Fantasy",
-        b: "Horror",
-        c: "Science Ficion",
-        d: "Mystery",
+            a: "Fantasy",
+            b: "Horror",
+            c: "Science Ficion",
+            d: "Mystery",
         },
-        correctAnswer : "c"
+        correctAnswer: "c"
     },
     {
-
-        question: "Which of Shakespears plays has a fairy named Puck?",
+        question: "Which of Shakespear's plays has a fairy named Puck?",
         answers: {
-        a: "The Tempest",
-        b: "Romeo and Juliet",
-        c: "Othello",
-        d: "A Midsummer Nights Dream",
+            a: "The Tempest",
+            b: "Romeo and Juliet",
+            c: "Othello",
+            d: "A Midsummer Nights Dream",
         },
-        correctAnswer : "d"
+        correctAnswer: "d"
     },
     {
-
-        question: "Beatrix Potter wrote and illustrated what popular childrens books?",
+        question: "Beatrix Potter wrote and illustrated what popular childrens' books?",
         answers: {
-        a: "The Tales of Peter Rabbit",
-        b: "Whinnie The Pooh",
-        c: "If You Give a Mouse a Cookie",
-        d: "The Veleteen Rabbit",
+            a: "The Tales of Peter Rabbit",
+            b: "Whinnie The Pooh",
+            c: "If You Give a Mouse a Cookie",
+            d: "The Veleteen Rabbit",
         },
-        correctAnswer : "a"
+        correctAnswer: "a"
     },
     {
-
         question: "Nathaniel Hawthorne wrote this novel?",
         answers: {
-        a: "The Grapes of Wrath",
-        b: "The Great Gatsby",
-        c: "The Scarlett Letter",
-        d: "Moby Dick",
+            a: "The Grapes of Wrath",
+            b: "The Great Gatsby",
+            c: "The Scarlett Letter",
+            d: "Moby Dick",
         },
-        correctAnswer : "c"
+        correctAnswer: "c"
     },
+];
 
-    ];
+function buildQuiz() {
+    const output = [];
 
-    buildQuiz();
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        const answers = [];
 
-    submitButton.addEventListener("click", showResults);
-})();
+        for (letter in currentQuestion.answers) {
+            answers.push(`
+                <label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${letter} : ${currentQuestion.answers[letter]}
+                </label>
+            `);
+        }
 
-//timer code
+        output.push(`
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+        `);
+    });
+
+    quizContainer.innerHTML = output.join("");
+}
+
+// shows a numerical value for how many were correct
+// changes the color of text based on which ones are right or wrong
+function showResults() {
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+    let numCorrect = 0;
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if (userAnswer === currentQuestion.correctAnswer) {
+            numCorrect++;
+            answerContainers[questionNumber].style.color = "lightgreen";
+        } else {
+            answerContainers[questionNumber].style.color = "red";
+        }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+    let timer = duration;
+    intervalId = setInterval(function () {
+        let minutes = parseInt(timer / 60, 10)
+        let seconds = parseInt(timer % 60, 10);
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        display.textContent = `${minutes}:${seconds}`;
 
         if (--timer < 0) {
             timer = duration;
@@ -173,10 +149,17 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-window.onload = function () {
-    var fiveMinutes = 60 * 4,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+function stopTimer() {
+    clearInterval(intervalId);
+}
 
-$("#submit").on("click", timer.stop);
+window.onload = function () {
+    const timerDuration = 60 * 3, // three minutes
+        display = document.querySelector('#time');
+    startTimer(timerDuration, display);
+    buildQuiz();
+    submitButton.addEventListener("click", () => {
+        stopTimer();
+        showResults();
+    });
+};
